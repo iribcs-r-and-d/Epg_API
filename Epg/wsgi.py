b7bf63f17e0903b222de370970d49459404c9b53
KavehRS <me@kavehrs.com>
2020-05-06 17:12:38 +0430

    update requirements.txt

--- Epg/wsgi.py
+++ Epg/wsgi.py
@@ -4,11 +4,7 @@ from django.core.wsgi import get_wsgi_application
 from Aban.views import NoteAPIView
 
 class NoteAPIView(WSRoot):
-    @expose(unicode, unicode)  # First parameter is the return type,
-                               # then the function argument types
-
-       
-
+    @expose(unicode, unicode) 
+    
 ws=NoteAPIView(protocols=['restjson', 'restxml', 'soap'])
 application = ws.wsgiapp()
-
