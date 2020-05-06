96103e1df76133cf0f97aa0e0408c707a6f158d9
Kaveh RezaeiShiraz <me@kavehrs.com>
2020-05-06 15:33:37 +0430

    clean code from commented lines

--- Aban/models.py
+++ Aban/models.py
@@ -14,22 +14,18 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 # Create your models here.
-# api/models.py
-
 
 class NoteAPIView(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['channel','program']
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
-
-   
-   
-class Note(models.Model):
-    channel = models.CharField(max_length=200)
-    program = models.CharField(max_length=200)
-    start_date = models.DateTimeField()
-    duration=models.IntegerField()
-    visit=models.IntegerField(max_length=200)
-    month=models.CharField(max_length=200)
+ 
+#class Note(models.Model):
+ #   channel = models.CharField(max_length=200)
+  #  program = models.CharField(max_length=200)
+  #  start_date = models.DateTimeField()
+  #  duration=models.IntegerField()
+  #  visit=models.IntegerField(max_length=200)
+  #  month=models.CharField(max_length=200)
 
