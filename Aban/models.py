d9b4f2d1fee60b5dc742986f929e8a71e6c95758
Kaveh RezaeiShiraz <me@kavehrs.com>
2020-05-09 10:40:13 +0430

    edit model for solve circular Refrence

--- Aban/models.py
+++ Aban/models.py
@@ -4,14 +4,13 @@ from django.db import models
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from .serializers import NoteSerializer
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 # Create your models here.
 
- 
+
 class Note(models.Model):
     channel = models.CharField(max_length=200)
     program = models.CharField(max_length=200)
@@ -19,4 +18,3 @@ class Note(models.Model):
     duration=models.IntegerField()
     visit=models.IntegerField(max_length=200)
     month=models.CharField(max_length=200)
-
