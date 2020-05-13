3bd5b57f9b0c7424695da8a453cccb41e1f317fa
Kaveh RezaeiShiraz <me@kavehrs.com>
2020-05-13 18:54:18 +0430

    edit int in model

--- Aban/models.py
+++ Aban/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Note(models.Model):
-    channel = models.CharField()
-    program = models.CharField()
+    channel = models.CharField(max_length=200)
+    program = models.CharField(max_length=200)
     start_date = models.DateTimeField()
     duration = models.IntegerField()
     visit = models.IntegerField()
-    month = models.CharField()
+    month = models.CharField(max_length=200)
