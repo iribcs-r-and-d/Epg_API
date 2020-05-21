2e84c3d7223eab220e70f0f578520f649e9a1f2e
Ali.NET <aliemami.net@gmail.com>
2020-05-21 13:00:37 +0430

    EPG-0002: Refactor project structure

--- Epg/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from spyne.server.django import DjangoView
-from Aban.views import Epg_service, app
-
-urlpatterns = [
-    url(r'^data/', Epg_service),
-    url(r'^api/', DjangoView.as_view(application=app)),
-]
